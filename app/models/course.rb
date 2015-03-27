7ae5253f96267d9f448afe1d6322cb6972e79a65
Brian Chau <brian-chau@uiowa.edu>
2015-03-26 23:20:26 -0500

    Contraints on new courses.

--- app/models/course.rb
+++ app/models/course.rb
@@ -4,4 +4,14 @@ class Course < ActiveRecord::Base
 	has_many :folders
 	belongs_to :semester
 	belongs_to :department
+
+	validates :title, presence: true
+	validates :department_id, presence: true
+	validates :semester_id, presence: true
+	validates :description, presence: true
+	validates :capacity, presence: true
+	validates :course_number, presence: true
+	validates :instructor_id, presence: true
+
+	validates :course_number, :uniqueness => {:scope => [:department_id, presence: true]}
 end
