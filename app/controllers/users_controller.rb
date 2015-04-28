df6a0a594fca44f1bb5331acaef13b63ef3008c7
Brian Chau <brian-chau@uiowa.edu>
2015-04-28 17:22:19 -0500

    Updated flash

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -22,7 +22,7 @@ class UsersController < ApplicationController
 		@user.update_attributes(secure_params)
 		respond_to do |format|
 			format.html {redirect_to user_path(@user.id) }
-			format.js { flash[:notice] = "Updated user!" }
+			format.js { flash.now[:notice] = "Updated user!" }
 		end
   end
 
